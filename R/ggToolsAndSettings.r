af911c9bc16b99643ad2d012c4deb585574efacd
bleutner <benjamin.leutner@uni-wuerzburg.de>
2014-11-04 10:30:00 +0100

    ggR can return the data.frame instead of the ggplot object

--- R/ggToolsAndSettings.r
+++ R/ggToolsAndSettings.r
@@ -5,19 +5,24 @@
 #' @param lowColor Character. Color for lowest value
 #' @param highColor Character. Color for highest value
 #' @param legendName Character. Layer name
+#' @param ggObj Logical. Return a ggplot2 object (TRUE) or just the data.frame
 #' 
 #'  @export 
-ggR <- function(x, layer = 1, maxpixels = 5000000, lowColor = "white", highColor = "black", legendName = "Legend") {  
+ggR <- function(x, layer = 1, maxpixels = 5000000, lowColor = "white", highColor = "black", legendName = "Legend", ggObj = TRUE) {  
     drast <- sampleRegular(x[[layer]], maxpixels, asRaster = TRUE)
     df <- data.frame(coordinates(drast), drast[])
     colnames(df) <- c("x", "y", names(x[[layer]]))
     layer <- colnames(df)[3]
   #  df <- melt(df, id.vars = c("x","y"))
     
-    ggplot(df) + geom_raster(aes_string(x = "x", y = "y", fill = layer)) +
+    if(ggObj) {p <- ggplot(df) + geom_raster(aes_string(x = "x", y = "y", fill = layer)) +
             scale_fill_gradient(low = lowColor, high = highColor, na.value = NA, name = legendName) +
             coord_equal() +
             MAPTHEME
+} else {
+    p <- df
+}
+return(p)
 }
 
 #' Theme for map plotting
